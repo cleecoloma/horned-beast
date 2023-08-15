e659805f88d3b115f99ed40c3369eab47109ac30
Chester Lee Coloma <cleecoloma@gmail.com>
2023-08-15 12:50:18 -0700

    added favorite count functionality

--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,16 +1,37 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { HeartFill } from 'react-bootstrap-icons';
 
 class HornedBeast extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { favoriteCount: 0, };
   }
+  handleClick = () => {
+    this.setState({favoriteCount: this.state.favoriteCount+1});
+  }
+
   render() {
     return (
-      <div>
-        <h2>{this.props.title}</h2>
-        <img src={this.props.image_url} alt={this.props.keyword} title={this.props.title}></img>
-        <p>{this.props.description}</p>
-      </div>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img
+          variant="top"
+          src={this.props.image_url}
+          alt={this.props.keyword}
+          title={this.props.title}
+        />
+        <Card.Body>
+          <Card.Title>{this.props.title}</Card.Title>
+          <Card.Text>{this.props.description}</Card.Text>
+          <Card.Text>
+            <HeartFill /> {this.state.favoriteCount}
+          </Card.Text>
+          <Button variant="primary" onClick={this.handleClick}>
+            Favorite
+          </Button>
+        </Card.Body>
+      </Card>
     );
   }
 }
