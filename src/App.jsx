ea38f61832ac96cb8e6431b753428e50f150ec8d
Chester Lee Coloma <cleecoloma@gmail.com>
2023-08-16 12:43:09 -0700

    added data.json to App and displays cards

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,13 @@ import Header from './components/Header';
 import Gallery from './components/Gallery';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import data from '../assets/data.json';
+import hornedBeastData from './assets/data.json';
 
 function App() {
-  
   return (
     <>
       <Header />
-      <Gallery />
+      <Gallery handleData={hornedBeastData}/>
       <Footer />
     </>
   );
--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import HornedBeast from './HornedBeast';
-// import data from '../assets/data.json';
 
 class Gallery extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div>
-        {data.map((item, _id) => 
-          <HornedBeast key={_id} title={item.title} image_url={item.image_url} keyword={item.keyword} description={item.description} />
-        )}
+        {this.props.handleData.map((item, _id) => (
+          <HornedBeast
+            key={_id}
+            title={item.title}
+            image_url={item.image_url}
+            keyword={item.keyword}
+            description={item.description}
+          />
+        ))}
       </div>
     );
   }
